1ddb9785d37749c6b6bab8df236b6cb53a0a1d7a
Y4nn1ckD <12400768@student.pxl.be>
2026-01-12 21:18:54 +0100

    "Grafische projecten" 50% af + poging tot fixen reponsivity

---
+++ grafischeprojecten.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('show');
+});
